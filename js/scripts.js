6ca1e9463390d809319552e380e50b7e822c7dc8
tubvision <87477436+yunanLanga@users.noreply.github.com>
2022-12-29 08:41:07 -1200

    upload da pagina

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+/*var cardP = document.querySelector(".cardP");
+cardP.addEventListener("onmouseover", function(e) {
+
+    */
+    var imgem = document.querySelector(".img1");
+    imgem.addEventListener("onmouseover",function(e){
+        this.style.overflow = "hidden";        
+        this.style.backgroundColor = "Red"; 
+    });
+    
